56a28eec5fd684b65902fbc00b12a7fd8c3bfaf2
richbutterfly <rich.butterfly04@gmail.com>
2023-10-19 18:46:17 +0100

    Java Homework Writing Khyati Savla

---
+++ src/javahomework/Programme1_Biodata.java
@@ -0,0 +1,21 @@
+package javahomework;
+
+//1. Write a program that prints your BIO Data.
+public class Programme1_Biodata {
+    public static void main(String[] args) {
+        System.out.println("             BIO DATA          ");
+        System.out.println("Name           :Jane Smith     ");
+        System.out.println("MObile         :07912345678    ");
+        System.out.println("Email          :janesmith@gmail.com");
+        System.out.println("Gender         :Female          ");
+        System.out.println("Date of Birth  :01.05.2000      ");
+        System.out.println("Marital Status :Single          ");
+        System.out.println("Language Known :English, Spanish and German");
+        System.out.println("Qualification  :Software Tester ");
+        System.out.println("Experience     :Agile tester with 5 years of experience");
+        System.out.println("Address        :123 Abc Street, London AB1 2CD");
+        System.out.println("Place          :London             ");
+        System.out.println("Date           :02.10.2021         ");
+        System.out.println("Signature      :JSmith             ");
+    }
+}
---
+++ src/javahomework/Programme2_PrintFace.java
@@ -0,0 +1,13 @@
+package javahomework;
+
+//2. Write a Java program to print a face.
+public class Programme2_PrintFace {
+    public static void main(String[] args) {
+        System.out.println(" + \" \" \" \" + ");
+        System.out.println("[|  o   o  |]");
+        System.out.println(" |    ^    | ");
+        System.out.println(" |  ' _ '  | ");
+        System.out.println(" +_________+ ");
+
+    }
+}
